6789bc270628426cabb29f5273719b09acd36b4d
zynpckmk <zeynepcakmak37@hotmail.com>
2021-06-05 12:11:39 +0300

    add route for editing Student information

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,17 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+use App\Http\Controllers\PostController;
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('tasks', [PostController::class, 'index']);
+
+Route::prefix('task')->group(function () {
+    Route::post('/store', [TaskController::class, 'store']);
+    Route::put('/{id}', [TaskController::class, 'update']);
+    Route::delete('/{id}', [TaskController::class, 'destroy']);
 });
